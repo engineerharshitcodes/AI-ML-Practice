00ea12140c8684dfafb361d73a29bf2f7d5c3583
engineerharshitcodes <engineerharsh99@gmail.com>
2026-08-05 02:39:32 +0530

    DQN

---
+++ Ml/miniProject/flappyBird/agent.py
@@ -0,0 +1,18 @@
+import flappy_bird_gymnasium
+import gymnasium
+env = gymnasium.make("FlappyBird-v0", render_mode="human", use_lidar=True)
+
+state, _ = env.reset()
+while True:
+    # Next action:
+    # (feed the observation to your agent here)
+    action = env.action_space.sample()
+
+    # Processing:
+    next_state, reward, terminated, _, _ = env.step(action)
+    
+    # Checking if the player is still alive
+    if terminated:
+        break
+
+env.close()
